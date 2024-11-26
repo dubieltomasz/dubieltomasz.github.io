543569f133f43c30b9c1f4dd0896d2db20c10142
Tomasz Dubiel <dubieltomasz420@gmail.com>
2024-11-27 00:28:31 +0100

    Update_27_11_24

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function changeMain(where){
-    $("#torso").load(where);
+function changeMain(path){
+    var url = window.location.href.split('?')[0];
+    window.location.href = url + "?q=" + path;
 }
