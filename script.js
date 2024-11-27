ae8369252bddca863be70873314f05478fc0bfc6
Tomasz Dubiel <dubieltomasz420@gmail.com>
2024-11-27 01:03:37 +0100

    Update3_27_11_24

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var url = window.location.href;
 
-function changeMain(path){
+function updateURLParams(path){
     window.location.href = url.split('?')[0] + "?q=" + path;
-
-    return false;
 }
 
-var q = new URL(url).searchParams.get('q');
-
-if(q != null)
-{
-    document.getElementById("torso").innerHTML='<object type="text/html" data="'+q+'.html" ></object>';
-}
-else
-{
-    document.getElementById("torso").innerHTML='aaa';
+function updateMain(){
+    var q = new URL(url).searchParams.get('q');
+    
+    if(q != null)
+    {
+        document.getElementById("torso").innerHTML='<object type="text/html" data="'+q+'.html" ></object>';
+    }
+    else
+    {
+        document.getElementById("torso").innerHTML='aaa';
+    }    
 }
